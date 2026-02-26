97540723a232fb3a34b70963e42cb3e18bb536c6
unknown <honey.badger1656@gmail.com>
2026-02-26 20:16:08 +0530

    Use python -m pip and uvicorn

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ set -e
 echo "Starting Container"
 
 cd Backend
-pip install --upgrade pip
-pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port "$PORT"
+
+# Use python to run pip
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+python -m uvicorn main:app --host 0.0.0.0 --port "$PORT"
